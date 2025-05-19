f77081fe37d2dbbce0e760383ee2bc2f411b3af3
invictus125 <50851487+invictus125@users.noreply.github.com>
2025-05-19 18:17:49 -0500

    Remove named database, update scripts, add dockerfile

--- zufechten-db/schema/full_teardown.sql
+++ zufechten-db/schema/full_teardown.sql
@@ -1,1 +1,22 @@
-DROP DATABASE Zufechten;
+DROP INDEX penalty_fencer_type;
+DROP INDEX match_update_fencer_type;
+
+DROP TABLE MatchFencerPenalty;
+DROP TABLE MatchUpdate;
+
+DROP TABLE EventFencer;
+DROP TABLE MatchFencer;
+DROP TABLE ElimMatch;
+DROP TABLE PoolMatch;
+
+DROP TABLE Fencer;
+DROP TABLE Match;
+DROP TABLE Eliminations;
+DROP TABLE Pool;
+DROP TABLE Event;
+DROP TABLE TournamentClub;
+DROP TABLE Tournament;
+
+DROP TABLE UserClub;
+DROP TABLE Club;
+DROP TABLE User;
